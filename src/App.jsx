4dfa92f473b87ec56fe7af55e5686212378eaa7a
hzfhzf137 <hzfhzf137@gmail.com>
2024-08-08 01:02:58 +0500

    navigate included in routes
    navigate included in rotes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
 import Home from './components/home.jsx';
 import CosmoCommunication from './components/cosmoCommunication.jsx';
@@ -8,14 +8,17 @@ import ContactUs from './components/contactUs.jsx';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cosmo-communication" element={<CosmoCommunication />} />
-        <Route path="/work" element={<Work />} />
-        <Route path="/contact-us" element={<ContactUs />} />
-      </Routes>
-    </Router>
+    <React.Fragment>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cosmo-communication" element={<CosmoCommunication />} />
+          <Route path="/work" element={<Work />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+          <Route path="*" element={<Navigate to="/" />} /> {/* Redirects all unknown routes to home */}
+        </Routes>
+      </Router>
+    </React.Fragment>
   );
 }
 
