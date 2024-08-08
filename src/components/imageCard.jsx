8b4cf08afb59e76b38f4be152735ea6eb79c664f
hzfhzf137 <hzfhzf137@gmail.com>
2024-08-09 00:19:59 +0500

    h
    j

--- src/components/imageCard.jsx
+++ src/components/imageCard.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 
 const ImageCard = ({ imageUrl }) => {
     return (
-        <div className="w-full sm:w-1/4 md:w-1/5 lg:w-1/6 px-1 pb-2">  {/* Adjusted responsive width for narrower container */}
-            <div className="h-48 md:h-52 lg:h-56 bg-gray-200 overflow-hidden relative group">  {/* Fixed height container with hover group */}
+        // Adjusted responsive width: wider for small screens, narrower for larger screens
+        <div className="w-1/2 sm:w-1/3 md:w-1/5 lg:w-1/6 px-1 pb-2">
+            <div className="h-48 md:h-52 lg:h-56 bg-gray-200 overflow-hidden relative group">
                 <img
                     src={imageUrl}
                     alt="Creation"
-                    className="w-full h-full transform group-hover:scale-105 transition-transform duration-300 ease-in-out"  /* Image covers entire container */
+                    className="w-full h-full transform group-hover:scale-105 transition-transform duration-300 ease-in-out"
                 />
-                <div className="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-30 transition-all duration-300 ease-in-out"></div> {/* Dark overlay on hover */}
+                <div className="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-30 transition-all duration-300 ease-in-out"></div>
             </div>
         </div>
     );
