f3dff5b84bb757379df055ab8d75b6bad9df6488
hzfhzf137 <hzfhzf137@gmail.com>
2024-08-10 01:25:40 +0500

    n
    n

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,8 +3,8 @@ import { FaWhatsapp } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 w-screen py-6">
-      <div className="container mx-auto px-4 flex items-center justify-start">
+    <footer className="bg-gray-900 w-screen py-6 ">
+      <div className="container mx-auto px-4 flex items-center justify-start pt-2 pb-2">
         <a 
           href="https://wa.me/03008423278"
           className="flex items-center text-gray-300 text-xs sm:text-sm md:text-base mr-2 hover:text-green-300"
@@ -20,7 +20,7 @@ const Footer = () => {
           <FaWhatsapp size={30} />
         </a>
       </div>
-      <p className="text-gray-300 text-xs sm:text-sm md:text-base text-center mt-2 pointer-events-none">
+      <p className="text-gray-300 text-xs sm:text-sm md:text-base text-center mt-2 pt-2 pointer-events-none">
         © 2024 Cosmo Communication. All rights reserved.
       </p>
     </footer>
